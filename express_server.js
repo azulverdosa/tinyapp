42f5ad0fd2bb02d0d14ba165316bb95859d3d29c
AvaCado <eliserichardsson@gmail.com>
2022-03-09 17:39:14 -0500

    add shorturl GET endpoint

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
-  "jh9h2k": "https://elkevogelsang.com/#&gid=1&pid=3"
+  "jh9h2k": "https://www.thisiscolossal.com/2014/03/absurdly-expressive-dog-portraits-by-elke-vogelsang/"
 };
 
 app.get("/", (req, res) => {
@@ -17,6 +17,13 @@ app.get("/", (req, res) => {
 app.get("/urls", (reu, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const { shortURL } = req.params;
+  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
+  
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
