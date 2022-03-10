9c6a614f577b608f28b88a7b5d3fa5116a0de82a
AvaCado <eliserichardsson@gmail.com>
2022-03-10 17:08:36 -0500

    added function to generate a random ShortURL

--- express_server.js
+++ express_server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,9 +20,14 @@ app.get("/", (req, res) => {
   res.send("Hello! Welcome to TinyApp :)");
 });
 
-app.get("/urls", (reu, res) => {
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
