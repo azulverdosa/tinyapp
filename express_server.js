5f5673be9ce19a2f39c4e883d59bae675181050c
AvaCado <eliserichardsson@gmail.com>
2022-03-12 14:06:06 -0500

    added logout button and added functionality to clear cookies

--- express_server.js
+++ express_server.js
@@ -29,6 +29,12 @@ app.post("/login", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/logout", (req, res) => {
+  
+  res.clearCookie("username");
+  res.redirect('/urls');
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase,
