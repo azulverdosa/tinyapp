4a9ade31d552a936e501c66d50e8f79ec673f3d1
AvaCado <eliserichardsson@gmail.com>
2022-03-22 17:46:29 -0400

    added helped functions

--- helpers.js
+++ helpers.js
@@ -1,5 +1,24 @@
+const findUrlsForUserId = (userId, database) => {
+  const arrOfUrlEntries = Object.entries(database); 
+
+  return arrOfUrlEntries.reduce((acc, [shortUrl, urlObj]) => {
+    if (urlObj.userId === userId) {
+      return {...acc, [shortUrl]: urlObj };
+    } else {
+      return acc;
+    }
+  }, {});
+};
+
 const findUserByEmail = (email, database) => Object.values(database).find(user => user?.email === email);
 
+const findUserById = (userId, database) => database[userId] || null;
+
+const generateRandomString = () => Math.random().toString(36).slice(2,8);
+
 module.exports = {
-  findUserByEmail
+  findUrlsForUserId,
+  findUserByEmail,
+  findUserById,
+  generateRandomString
 };
