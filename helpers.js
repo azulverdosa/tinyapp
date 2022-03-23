10625cfc266a69c8b023cd8f239bbdee0d6ea0e4
AvaCado <eliserichardsson@gmail.com>
2022-03-23 19:29:58 -0400

    applied Prettier

--- helpers.js
+++ helpers.js
@@ -1,24 +1,25 @@
 const findUrlsForUserId = (userId, database) => {
-  const arrOfUrlEntries = Object.entries(database); 
+  const arrOfUrlEntries = Object.entries(database);
 
   return arrOfUrlEntries.reduce((acc, [shortUrl, urlObj]) => {
     if (urlObj.userId === userId) {
-      return {...acc, [shortUrl]: urlObj };
+      return { ...acc, [shortUrl]: urlObj };
     } else {
       return acc;
     }
   }, {});
 };
 
-const findUserByEmail = (email, database) => Object.values(database).find(user => user?.email === email);
+const findUserByEmail = (email, database) =>
+  Object.values(database).find((user) => user?.email === email);
 
 const findUserById = (userId, database) => database[userId] || null;
 
-const generateRandomString = () => Math.random().toString(36).slice(2,8);
+const generateRandomString = () => Math.random().toString(36).slice(2, 8);
 
 module.exports = {
   findUrlsForUserId,
   findUserByEmail,
   findUserById,
-  generateRandomString
+  generateRandomString,
 };
